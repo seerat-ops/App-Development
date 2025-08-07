704b191d68e8b397474d7270f2c07e4f05c56128
Seerat Shaikh <seerat.edu.shaikh@gmail.com>
2025-08-07 10:06:47 +0530

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num1, num2, sum;
+
+	// Ask for input
+	cout << "Enter first number: ";
+	cin >> num1;
+
+	cout << "Enter second number: ";
+	cin >> num2;
+
+	// Calculate sum
+	sum = num1 + num2;
+
+	// Display result
+	cout << "Sum = " << sum << endl;
+
+	return 0;
+}
